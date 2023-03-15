52558506818f60e696b4816a0d4a6608e9ecde14
Zain-Asif-Falcon <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2023-03-15 17:14:02 +0500

    Add files via upload

---
+++ Assests/Scripts/RivalaryCarsPooler.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class RivalaryCarsPooler : MonoBehaviour
+{
+    public static RivalaryCarsPooler intance;
+    public List<GameObject> RivalaryCarsList = new List<GameObject>();
+    private List<GameObject> RivalaryCarsPool = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (intance == null) intance = this;
+        if (PlayerPrefs.GetString("GameMode") == GameConstants.Hard_SceneName)
+        {
+            InstantiateNewCarsInPool_Hard();
+            InstantiateNewCarsInPool_Hard();
+        }
+        else
+        {
+            InstantiateNewCarsInPool_Easy();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public GameObject GetRandomRivalryCar()
+    {
+        bool Flag = false;
+        GameObject RivalaryCar = RivalaryCarsPool[0];
+        for (int i = 0; i < RivalaryCarsPool.Count; i++)
+        {
+            if (!RivalaryCarsPool[i].activeInHierarchy)
+            {
+                RivalaryCar = RivalaryCarsPool[i];
+                Flag = true;
+                break;
+            }
+        }
+        if (Flag) return RivalaryCar;
+        else
+        {
+            GameObject NewRivalryCar = Instantiate(RivalaryCarsList[Random.Range(0, RivalaryCarsList.Count)]);
+            NewRivalryCar.SetActive(false);
+            RivalaryCarsPool.Add(NewRivalryCar);
+            return NewRivalryCar;
+        }
+    }
+    private void InstantiateNewCarsInPool_Hard()
+    {
+        foreach (var Car in RivalaryCarsList)
+        {
+            GameObject RivalryCar = Instantiate(Car);
+            RivalryCar.SetActive(false);
+            RivalaryCarsPool.Add(RivalryCar);
+        }
+    }
+    private void InstantiateNewCarsInPool_Easy()
+    {
+        // int CarsCount = 0;
+        // foreach (var Car in RivalaryCarsList)
+        // {
+        //     GameObject RivalryCar = Instantiate(Car);
+        //     RivalryCar.SetActive(false);
+        //     RivalaryCarsPool.Add(RivalryCar);
+        //     CarsCount += 1;
+        //     if (CarsCount >= 6) break;
+        // }
+
+        foreach (var Car in RivalaryCarsList)
+        {
+            GameObject RivalryCar = Instantiate(Car);
+            RivalryCar.SetActive(false);
+            RivalaryCarsPool.Add(RivalryCar);
+        }
+    }
+}
+
+
